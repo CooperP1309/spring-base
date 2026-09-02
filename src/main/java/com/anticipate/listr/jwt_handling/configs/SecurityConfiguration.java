4fa05514e5d2d04d0ba755600105a91e51d51c9d
CooperP1309 <cooperpeek1309@gmail.com>
2026-09-02 10:56:56 +1000

    ADDED: Favicon and refactored sec config to allman style

--- src/main/java/com/anticipate/listr/jwt_handling/configs/SecurityConfiguration.java
+++ src/main/java/com/anticipate/listr/jwt_handling/configs/SecurityConfiguration.java
@@ -16,20 +16,23 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration 
+{
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     public SecurityConfiguration(
         JwtAuthenticationFilter jwtAuthenticationFilter,
         AuthenticationProvider authenticationProvider
-    ) {
+    ) 
+    {
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception 
+    {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
@@ -48,7 +51,8 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    CorsConfigurationSource corsConfigurationSource() 
+    {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:8005"));
