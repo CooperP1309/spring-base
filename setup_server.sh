98bdde25f82ce7896cf4ce93fd1ec4829a261f30
CooperP1309 <cooperpeek1309@gmail.com>
2026-09-04 18:10:49 +1000

    REFACTORED and FIXED: Setup script to only require Java now that mvnw file is working + added startup script with container health probing

--- setup_server.sh
+++ setup_server.sh
@@ -322,6 +322,8 @@ logging.level.org.springframework.security.config.annotation.authentication.conf
 spring.datasource.url=jdbc:mysql://localhost:$db_port/taskdb?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false&createDatabaseIfNotExist=true
 spring.datasource.username=root
 spring.datasource.password=$db_password
+# Name of the Docker container running MySQL (used by start_server.sh)
+db.container.name=$db_container_name
 
 ## Hibernate properties
 spring.jpa.hibernate.ddl-auto=update
@@ -404,4 +406,4 @@ echo "SMTP host:          $smtp_host:$smtp_port"
 echo
 echo "To start the server, run the following command:"
 echo
-echo "mvn spring-boot:run"
+echo "./start_server.sh"
