5ebd6a4a450a3f80958097358ca1a7debddca5d2
CooperP1309 <cooperpeek1309@gmail.com>
2026-09-24 15:52:54 +1000

    ADDED: Session token rejection for disabled accounts

--- src/main/java/com/anticipate/listr/jwt_handling/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/anticipate/listr/jwt_handling/configs/JwtAuthenticationFilter.java
@@ -58,7 +58,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             if (userEmail != null && authentication == null) {
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-                if (jwtService.isTokenValid(jwt, userDetails)) {
+                if (userDetails.isEnabled()
+                        && userDetails.isAccountNonLocked()
+                        && jwtService.isTokenValid(jwt, userDetails)) {
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                             userDetails,
                             null,
